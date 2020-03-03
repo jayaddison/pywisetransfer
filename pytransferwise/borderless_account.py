e3d038b3927570710e294368787064ec673de1f9
James Addison <jay@jp-hosting.net>
2020-03-03 12:33:45 +0000

    Reformat via black

--- pytransferwise/borderless_account.py
+++ pytransferwise/borderless_account.py
@@ -4,9 +4,7 @@ from pytransferwise.base import Base
 
 
 class BorderlessAccount(Base):
-    list = JsonEndpoint(
-        path="/v1/borderless-accounts", required_params=["profileId"]
-    )
+    list = JsonEndpoint(path="/v1/borderless-accounts", required_params=["profileId"])
     statement = JsonEndpoint(
         path="/v3/profiles/{profileId}/borderless-accounts/{borderlessAccountId}/statement.json",
         required_params=["currency", "intervalStart", "intervalEnd"],
