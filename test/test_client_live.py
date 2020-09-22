101d6b843110651cc83359de9f1d885d3be3f827
James Addison <jay@jp-hosting.net>
2020-09-22 16:50:54 +0100

    Live client test: do not perform pre-test sandbox setup

--- test/test_client_live.py
+++ test/test_client_live.py
@@ -1,12 +1,5 @@
-import pytransferwise
-
-
-def setup_function(method):
-    pytransferwise.api_key = None
-    pytransferwise.environment = "sandbox"
-
-
 def test_client_live_environment():
+    import pytransferwise
     pytransferwise.api_key = "live-key"
     pytransferwise.environment = "live"
     client = pytransferwise.Client()
