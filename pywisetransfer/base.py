071817c262dc3f7529bf015b5f24372f430ae3eb
James Addison <jay@jp-hosting.net>
2021-12-26 13:41:46 +0000

    Add explanatory comment regarding 'client' argument and why it is kwargs-optionalized

--- pywisetransfer/base.py
+++ pywisetransfer/base.py
@@ -6,6 +6,11 @@ class Base(Service):
     client = None
 
     def __init__(self, *args, **kwargs):
+        # TODO: It would be nice to make 'client' a required argument here
+        # Currently apiron performs a zero-argument constructor call to the
+        # service base class to retrieve the 'required_headers' property,
+        # so we cater for empty arguments here
+        # https://github.com/ithaka/apiron/blob/v5.1.0/src/apiron/service/base.py#L7
         if "client" in kwargs and Base.client != kwargs["client"]:
             Base.client = kwargs["client"]
         if Base.client and Base.client.environment == "live":
