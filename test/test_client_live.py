7e626b0e42e5908b42d8ba871132ac70f9fea00d
James Addison <jay@jp-hosting.net>
2021-12-26 13:33:47 +0000

    Add additional responses-based test coverage on live-mode client API calls

--- test/test_client_live.py
+++ test/test_client_live.py
@@ -1,7 +1,44 @@
-def test_client_live_environment():
+import pytest
+import responses
+
+
+@pytest.fixture
+def me_response():
+    return {
+        "id": 101,
+        "name": "Example Person",
+        "email": "person@example.com",
+        "active": True,
+        "details": {
+            "firstName": "Example",
+            "lastName": "Person",
+            "phoneNumber": "+37111111111",
+            "occupation": "",
+            "address": {
+                "city": "Tallinn",
+                "countryCode": "EE",
+                "postCode": "11111",
+                "state": "",
+                "firstLine": "Road 123",
+            },
+            "dateOfBirth": "1977-01-01",
+            "avatar": "https://lh6.googleusercontent.com/photo.jpg",
+            "primaryAddress": 111,
+        },
+    }
+
+
+@responses.activate
+def test_client_live_environment(me_response):
+    responses.add(
+        responses.GET,
+        "https://api.transferwise.com/v1/me",
+        json=me_response,
+    )
     import pywisetransfer
 
     client = pywisetransfer.Client(api_key="live-key", environment="live")
+    me = client.users.me()
 
     domain = client.borderless_accounts.service.domain
     assert "api.sandbox.transferwise" not in domain
