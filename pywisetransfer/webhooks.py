2e92543b08abb98167c87c21bce282efa0285960
James Addison <jay@jp-hosting.net>
2023-09-14 22:13:28 +0100

    cleanup: remove unused import

--- pywisetransfer/webhooks.py
+++ pywisetransfer/webhooks.py
@@ -8,7 +8,7 @@ from pywisetransfer.keys import (
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding, utils
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
 
