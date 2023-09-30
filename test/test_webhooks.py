d12ff4bab06f039ff7dd24104abb964fe073797c
James Addison <jay@jp-hosting.net>
2023-09-30 01:23:03 +0100

    Cleanup: minify diff

--- test/test_webhooks.py
+++ test/test_webhooks.py
@@ -18,29 +18,32 @@ def corrupt_payload():
 
 
 @pytest.fixture
-def valid_signature_header():
+def valid_signature():
     return "EMRety5CM0VqStb6bIeB2DeQwdAO5Nm06ZSSGT0/8qW4+cbYDaPwFa5bL1Ylkn/E/JqKhZNtydeT0x+z+nkKCqlFx3Mt/K/WCSD9t+stoZa/Viv8GY8gVzt20A//2+mg0lqCdob5KFCBGHa7GRAwpO4WR5i5reBo17xubq6uVCB8/4hEUVDEstX1m32TU1OES1pFwWECCE/uFPoVJ+5h9BFqnVLSh2XnJde+9aHZ1N+nPBljZWCi9Z+iPpr1MFtHJdjGMPol8+i0VGzi02nhiHUNghmK4uIajB3rledPDZ0MgkQ3wfzJuzHcKGrb0iUMwAsOUNyEJT3b0/g4J7Ka+Q=="
 
 
 @pytest.fixture
-def corrupt_signature_header():
+def corrupt_signature():
     return "EMRety5CM0VqStb6bIeB2DeQwdAO5Nm06ZSSGT0/8qW4+cbYDaPwFa5bL1Ylkn/E/JqKhZNtydeT1x+z+nkKCqlFx3Mt/K/WCSD9t+stoZa/Viv8GY8gVzt20A//2+mg0lqCdob5KFCBGHa7GRAwpO4WR5i5reBo17xubq6uVCB8/4hEUVDEstX1m32TU1OES1pFwWECCE/uFPoVJ+5h9BFqnVLSh2XnJde+9aHZ1N+nPBljZWCi9Z+iPpr1MFtHJdjGMPol8+i0VGzi02nhiHUNghmK4uIajB3rledPDZ0MgkQ3wfzJuzHcKGrb0iUMwAsOUNyEJT3b0/g4J7Ka+Q=="
 
 
-def test_correct_signature(valid_payload, valid_signature_header):
-    assert verify_signature(valid_payload, b64decode(valid_signature_header))
+def test_correct_signature(valid_payload, valid_signature):
+    result = verify_signature(valid_payload, b64decode(valid_signature))
+    assert result is True
 
 
-def test_corrupt_payload(corrupt_payload, valid_signature_header):
-    assert not verify_signature(corrupt_payload, b64decode(valid_signature_header))
+def test_corrupt_payload(corrupt_payload, valid_signature):
+    result = verify_signature(corrupt_payload, b64decode(valid_signature))
+    assert result is False
 
 
-def test_corrupt_signature(valid_payload, corrupt_signature_header):
-    assert not verify_signature(valid_payload, b64decode(corrupt_signature_header))
+def test_corrupt_signature(valid_payload, corrupt_signature):
+    result = verify_signature(valid_payload, b64decode(corrupt_signature))
+    assert result is False
 
 
 @pytest.fixture
-def valid_request(valid_payload, valid_signature_header):
+def valid_request(valid_payload, valid_signature):
     # Note: we construct an HTTP _client_ requests.Request object here; the
     # argument received at runtime will be the webserver's representation of
     # a request that it has received, and that interface may differ.
@@ -49,7 +52,7 @@ def valid_request(valid_payload, valid_signature_header):
         url="http://example.org",
         headers={
             "Content-Type": "application/json",
-            "X-Signature-SHA256": valid_signature_header,
+            "X-Signature-SHA256": valid_signature,
         },
         data=valid_payload,
         json=json.loads(valid_payload),
