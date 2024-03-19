af0d023d286a81a6e2670c1c44288b8cfa03d898
James Addison <jay@jp-hosting.net>
2024-03-19 12:42:20 +0000

    Fixup: balances endpoint accepts a list of types as a parameter.

--- pywisetransfer/balances.py
+++ pywisetransfer/balances.py
@@ -8,7 +8,7 @@ from pywisetransfer.base import Base
 
 
 class BalancesService(Base):
-    list = JsonEndpoint(path="/v4/profiles/{profile_id}/balances", required_params=["type"])
+    list = JsonEndpoint(path="/v4/profiles/{profile_id}/balances", required_params=["types"])
     get = JsonEndpoint(path="/v4/profiles/{profile_id}/balances/{balance_id}")
 
 
@@ -16,12 +16,19 @@ class Balances:
     def __init__(self, client: Client):
         self.service = BalancesService(client=client)
 
-    def list(self, profile_id: str, type: str = "STANDARD") -> Any:
-        valid_types = ["STANDARD", "SAVINGS"]
-        if type not in valid_types:
-            raise ValueError(f"Invalid type '{type}'; value values are: {valid_types}")
+    def list(self, profile_id: str, types: str | list[str] = "STANDARD") -> Any:
+        if not isinstance(types, list):
+            assert isinstance(types, str)
+            types = [types]
 
-        return munchify(self.service.list(profile_id=profile_id, type=type))
+        valid_types = ["STANDARD", "SAVINGS"]
+        for value in types:
+            assert isinstance(value, str)
+            if value not in valid_types:
+                raise ValueError(f"Invalid type '{type}'; value values are: {valid_types}")
+
+        params = {"types": ",".join(types)}
+        return munchify(self.service.list(profile_id=profile_id, params=params))
 
     def get(self, profile_id: str, balance_id: str) -> Any:
         return munchify(self.service.get(profile_id=profile_id, balance_id=balance_id))
