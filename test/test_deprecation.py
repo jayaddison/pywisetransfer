7c9cd5b77e4d76299e0c49e5bb1945bdba01afca
James Addison <jay@jp-hosting.net>
2024-03-21 21:40:39 +0000

    Tests: parametrize decorator testing.

--- test/test_deprecation.py
+++ test/test_deprecation.py
@@ -1,7 +1,8 @@
 from warnings import catch_warnings
 
-from pywisetransfer.deprecation import deprecated
+import pytest
 
+from pywisetransfer.deprecation import deprecated
 
 record_warnings = lambda: catch_warnings(record=True)
 
@@ -20,67 +21,39 @@ def zero_args_decorator():
     return 1
 
 
-@deprecated("test message")
+@deprecated("positional")
 def posarg_decorator():
     return 1
 
 
-@deprecated(message="test message")
+@deprecated(message="keyword")
 def kwarg_decorator():
     return 1
 
 
-def test_no_decorator():
-    actual_repr = repr(undecorated)
+@pytest.mark.parametrize(
+    "func, name, deprecated, message",
+    [
+        (undecorated, "undecorated", False, None),
+        (bare_decorator, "bare_decorator", True, None),
+        (zero_args_decorator, "zero_args_decorator", True, None),
+        (posarg_decorator, "posarg_decorator", True, "positional"),
+        (kwarg_decorator, "kwarg_decorator", True, "keyword"),
+    ],
+)
+def test_no_decorator(func, name, deprecated, message):
+    actual_repr = repr(func)
     with record_warnings() as ws:
-        result = undecorated()
+        result = func()
 
-    assert "deprecated" not in actual_repr
+    # Check the Python repr of the function
+    assert name in actual_repr
+    actual_deprecated = "deprecated" in actual_repr
+    assert deprecated == actual_deprecated
+
+    # Check the behaviour of the function
     assert result == 1
-    assert not ws
 
-
-def test_decorator_bare():
-    actual_repr = repr(bare_decorator)
-    with record_warnings() as ws:
-        result = bare_decorator()
-
-    assert "deprecated" in actual_repr
-    assert "function bare_decorator" in actual_repr
-    assert result == 1
-    assert ws
-
-
-def test_decorator_zero_args():
-    actual_repr = repr(zero_args_decorator)
-    with record_warnings() as ws:
-        result = zero_args_decorator()
-
-    assert "deprecated" in actual_repr
-    assert "function zero_args_decorator" in actual_repr
-    assert result == 1
-    assert ws
-
-
-def test_decorator_with_args():
-    actual_repr = repr(posarg_decorator)
-    with record_warnings() as ws:
-        result = posarg_decorator()
-
-    assert "deprecated" in actual_repr
-    assert "function posarg_decorator" in actual_repr
-    assert result == 1
-    assert ws
-    assert any([str(w.message) == "test message" for w in ws])
-
-
-def test_decorator_with_kwargs():
-    actual_repr = repr(kwarg_decorator)
-    with record_warnings() as ws:
-        result = kwarg_decorator()
-
-    assert "deprecated" in actual_repr
-    assert "function kwarg_decorator" in actual_repr
-    assert result == 1
-    assert ws
-    assert any([str(w.message) == "test message" for w in ws])
+    # Check the warnings emitted by the function
+    assert ws if deprecated else not ws
+    assert any([message in str(w.message) for w in ws] if message else [True])
