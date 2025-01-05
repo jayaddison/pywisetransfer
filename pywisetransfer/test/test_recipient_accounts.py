7a8114e0d4e8f512d69ee22a9b079e38b44951c5
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-01-05 19:49:02 +0000

    Check profile subset

--- pywisetransfer/test/test_recipient_accounts.py
+++ pywisetransfer/test/test_recipient_accounts.py
@@ -11,3 +11,14 @@ def test_same_ids_returned_when_page_size_differs(client: Client):
 def test_get_recipient(client: Client):
     """Successfully get a recipient account."""
     client.recipient_accounts.get(700614969)
+
+
+def test_listing_by_profile(client: Client):
+    """Successfully list recipient accounts by profile."""
+    p = {a.id for a in client.recipient_accounts.all(profile=client.profiles.personal)}
+    b = {a.id for a in client.recipient_accounts.all(profile=client.profiles.business[0])}
+    a = {a.id for a in client.recipient_accounts.all(page_size=4)}
+    assert p, "Personal profile is not empty."
+    assert b, "Business profile is not empty."
+    assert a == p | b, "All recipients are listed in both profiles."
+    
