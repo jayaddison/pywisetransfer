5c69104bc38858114d9542b9b3f029c2f702942d
James Addison <jay@jp-hosting.net>
2026-03-11 11:01:14 +0000

    Documentation: update `apiron` URL reference
    Relates-to commits 2539b87e42f4fd95bd564d355732d45aa57cf6e3...9f8123f5d6074cd91f7d90926adfc6afc8dfd310.

--- pywisetransfer/base.py
+++ pywisetransfer/base.py
@@ -16,7 +16,7 @@ class Base(Service):
         # Currently apiron performs a zero-argument constructor call to the
         # service base class to retrieve the 'required_headers' property,
         # so we cater for empty arguments here
-        # https://github.com/ithaka/apiron/blob/v5.1.0/src/apiron/service/base.py#L7
+        # https://github.com/ithaka/apiron/blob/v9.1.0/src/apiron/service/base.py#L9
         if "client" in kwargs and Base.client != kwargs["client"]:
             Base.client = kwargs["client"]
         if Base.client and Base.client.environment == "live":
